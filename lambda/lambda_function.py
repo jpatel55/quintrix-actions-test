d5c716cc31fcbd47e8e5e1b0ffcf82540c54f2b1
Jemil Patel <jemilpatel84@gmail.com>
2025-10-24 19:13:50 -0400

    created lambda

---
+++ lambda/lambda_function.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    print("Lambda received event:")
+    print(json.dumps(event))
+
+    # This just passes the S3 event details right through
+    return event
